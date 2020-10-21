287aa0160d9b2c31c9f90297b68b7d769c4556bc
Дарья Петрова <petrova.dv@phystech.edu>
2020-10-21 21:00:39 +0300

    Added hw 6

---
+++ w6/3.py
@@ -0,0 +1,39 @@
+from math import sin,sqrt
+
+
+class MyMath:
+    pi = 3.14
+    _complex = False #инкапсуляция  
+
+  # @classmethod
+   # def get_complex(cls):
+    #    return cls.__complex
+
+    @staticmethod
+    def get_sin(x):  #наследование, get_sin не реализован в дочернем классе, но мы можем его вызвать, как и get_sqrt
+        return sin(x)
+
+    @classmethod
+    def get_sqrt(cls,x):  #наследование
+        if x>=0:
+            return sqrt(x)
+        else:
+            if not cls._complex: #полиморфизм
+                return ValueError('Было введено отрицательное число')
+            else:
+                return (0, sqrt(-x)*2.0j)
+
+
+class MyComplexMath(MyMath): #наследование
+    _complex = True  #инкапсуляция 
+
+    #@classmethod
+    #def get_complex(cls):
+    #    return cls.__complex
+
+a = MyMath()
+b = MyComplexMath()
+print(a.get_sqrt(-1))
+print(b.get_sqrt(-1))
+
+#Мы использовали классический метод, так как он может получать доступ или менять состояние класса, а статистический - нет (поле _complex)
