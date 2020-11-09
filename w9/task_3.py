60ac4dad6a5730ddddab5875797985f00bec66f1
Дарья Петрова <petrova.dv@phystech.edu>
2020-11-09 23:03:32 +0300

    Add hw 9

---
+++ w9/task_3.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+class PrintAverage(Exception):
+    pass
+
+class PrintDispersion(Exception):
+    pass
+
+class PrintNumber(Exception):
+    pass
+
+def the_coroutine():
+    print("Starting coroutine")
+    data = []
+    try:
+        while True:
+            try:
+                x = yield
+                data.append(x)
+                average = np.mean(data)
+                disp = np.var(data)
+                n = len(data)
+            except PrintDispersion:
+                yield disp
+            except PrintAverage:
+                yield average
+            except PrintNumber:
+                yield n
+    finally:
+        print("Stop coroutine")
+
+coroutine = the_coroutine()
+next(coroutine)
+for i in range(12):
+    coroutine.send(i)
+    if i%2 == 0:
+        print("Average:", coroutine.throw(PrintAverage))
+        next(coroutine)
+    if i%3 == 0:
+        print("Dispersion:", coroutine.throw(PrintDispersion))
+        next(coroutine)
+    if i%4 == 0:
+        print("Number of data:", coroutine.throw(PrintNumber))
+        next(coroutine)
+
+
