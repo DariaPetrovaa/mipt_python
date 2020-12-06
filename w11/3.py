aa2ba28680e09c8539cfb1e493ad8e8e6f8aac53
Дарья Петрова <petrova.dv@phystech.edu>
2020-12-06 16:13:09 +0300

    Fix 3.py

--- w11/3.py
+++ w11/3.py
@@ -1,26 +1,31 @@
 import aiohttp
 import asyncio
-import aiofile
+from aiofile import LineReader, AIOFile, Writer
 import re
 
 
-async def func(session, service):
-    async with session.get(service) as response:
-        html = response.text()
-        html = re.split(r'[\r\n]', html)
-        for i in html:
-            if i.startswith('<a >'): 
-                async with aiofile.async_open('found.txt', 'w') as f:
-                    f.write(i)
-
-
-async def main(name):
-    async with aiofile.async_open(name, 'r') as f: 
-        async for line in aiofile.LineReader(f): 
+async def function(f1,f2):
+    async with AIOFile(f1, 'r') as f:
+        async for line in LineReader(f):
+            print(f'I\'m here: {line[:-2:]}')
             async with aiohttp.ClientSession() as session:
-                asyncio.ensure_future(func(session, line))
-        print('Finished')
+                async with session.get(line) as response:
+                    html = await response.text()
+                    html = re.split(r'[\r\n]', str(html))
+                    print(f'Lines starting with <a :')
+                    for i in html:
+                        if i.strip().startswith('<a '):
+                            print(i)
+                            async with AIOFile(f2, 'a') as f:
+                                writer = Writer(f)
+                                await writer(f'{i}\n')
+    print('Finished')
+
 
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main('urls.txt'))
+with open('res.txt','w') as f:
+    pass
+loop.run_until_complete(function("urls.txt",'res.txt'))
+
+
